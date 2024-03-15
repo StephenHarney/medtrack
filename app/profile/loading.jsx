2f346a049f00f7f418af2ba247c2e9df1c0aa0be
StephenHarney <g00367573@atu.ie>
2024-03-15 14:28:34 +0000

    first commit

---
+++ app/profile/loading.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+const loading = () => {
+  return (
+        <div>
+        
+        <Image
+            src='assets/icons/loader.svg'
+            width={50}
+            height={50}
+            alt='loader'
+            className='object-contain'
+        />
+    </div>
+  )
+}
+
+export default loading
---
+++ components/Feed.jsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { useState,useEffect } from 'react'; 
+
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({data, handleTagClick}) => {
+  return(
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+        key={post._id}
+        post={post}
+        handleTagClick={handleTagClick}
+        />
+
+      ))}
+    </div>
+  )
+}
+
+const Feed = () => {
+  const [searchText, setSearchText] = useState(''); 
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults,setSearchResults]  = useState([]);
+  const [posts, setPosts] = useState([]);
+  
+
+  const handleSearchChange = (e) =>{
+   e.preventDefault(searchTimeout);
+    searchText(e.traget.value);
+
+    
+  }
+  useEffect(() => {
+    const fetchPosts = async () =>{
+      const response = await fetch('/api/prompt'); 
+      const data = await response.json(); 
+
+      setPosts(data);
+
+
+    }
+  
+    fetchPosts();
+  }, [])
+  
+  const filterPrompts = (searchText) => {
+    const regex = new RegExp(searchText, "i");// 'i' flag for case-insensitive search
+    return posts.filter(
+      (p) =>
+      regex.test(p.creator.username) || 
+      regex.test(p.tag) ||
+      regex.test(p.prompt)
+
+    )
+
+  }
+
+  const handleTagClick = (tagName) => {
+    searchText(tagName);
+
+    const searchText = filterPrompts(tagName);
+    searchedResults(searchedResults);
+  }
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input 
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        
+        />
+      </form>
+      <PromptCardList 
+        data={posts}
+        handleTagClick={() => {}}
+      />
+
+    </section>
+  )
+}
+
+export default Feed
