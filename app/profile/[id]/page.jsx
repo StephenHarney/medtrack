f2d33311ca92b25dae42665f9ee22693fb7e64b8
StephenHarney <g00367573@atu.ie>
2024-04-22 11:32:21 +0100

    Add app

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,21 @@
+/*"use client";
+
+import { useEffec, useState } from "react";
+import { useSearchParams } from "next/navigation"; 
+
+import Profile
+
+from "@components/Profile";   
+
+import React from 'react'
+
+const dUserProfile = () => {
+  return (
+    <div> UserProfile
+
+    </div>
+  )
+}
+
+export default UserProfile;
+*/
