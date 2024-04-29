c95a93bbd606940ee606a18099f15bfb29b32acb
StephenHarney <g00367573@atu.ie>
2024-04-29 13:42:51 +0100

    Adding user profiles with params

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,1 +1,51 @@
 //To add other user profiles 
+"use client";
+
+import { useState, useEffect} from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation'
+import Profile  from '@components/Profile';
+
+
+const MyProfile = () => {
+    const router = useRouter();
+ 
+    const {data: session} = useSession();
+ 
+    const [myposts,setMyPosts] = useState([]);
+ 
+    useEffect(() => {
+        const fetchPosts = async () =>{
+          const response = await fetch(`api/users/${session.user.id}/posts`); 
+          const data = await response.json(); 
+      
+          setMyPosts(data);
+      
+      
+        }
+      
+            if(session?.user.id ) fetchPosts();
+         }, []);
+        
+       
+
+     
+     
+
+
+
+
+
+return (
+    <Profile 
+       name="My"
+       desc="Welcome to your personalized profile page"
+       data={myposts}
+       handleEdit={handleEdit}
+       handleDelete={handleDelete}
+
+    />
+ )  
+
+}
