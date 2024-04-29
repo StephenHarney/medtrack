90b30a6730552ed4103552ab063731df72e858cc
StephenHarney <g00367573@atu.ie>
2024-04-29 02:01:38 +0100

    Styling!!

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <h1 className="head_text text-center">
      Keep Track of Your Medicine &
       <br className="max-md:hidden" />
-      <span  className="orange_gradient text-center">Put Your Mind at Ease</span>
+      <span  className="purple_gradient text-center">Put Your Mind at Ease</span>
      </h1>
      <p className="Desc text-center">
       Medtrac is an app that helps you keep track of your daily Medicne intake!
