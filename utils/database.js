387a04de416b90dd6cc5b26b222f41a358436c69
StephenHarney <g00367573@atu.ie>
2025-04-30 19:16:23 +0100

    not shure

--- utils/database.js
+++ utils/database.js
@@ -5,11 +5,9 @@ let isConnected = false; // track the connection
 export const connectToDB = async () => {
   mongoose.set('strictQuery', true);
 
-<<<<<<< HEAD
   if(isConnected) {
-=======
+
   if(isConnected) {      
->>>>>>> 17e1b51f5bafa9bda9911bf99ebe70f3bda2a1d7
     console.log('MongoDB is already connected');
     return;
   }
@@ -26,4 +24,5 @@ export const connectToDB = async () => {
   } catch (error) {
     console.log(error);
   }
+ } 
 }
